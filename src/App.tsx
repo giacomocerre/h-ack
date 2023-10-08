5542145718b0a0be0bdbed085bc34261cb93c075
giacomocerre <cerre.giacomo93@gmail.com>
2023-10-08 05:39:08 +0200

    Add Suggest Page

--- src/App.tsx
+++ src/App.tsx
@@ -7,39 +7,49 @@ import HomeComponent from './components/home.component';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ChatComponent from './components/chat.component';
 import logo from "./assets/img/logo.png"
+import SuggestList from './components/suggest.component';
 
 
 
 const { MEMORY_TWIN_ID, MEMORY_TWIN_PASSWORD } = env
 
 function App() {
-  const contextValue = useContext(ThemeContext);
-  console.log(contextValue)
-  if (!contextValue) {
-    // Handle the case where the context value is undefined
-    return <div>Loading...</div>;
-  }
 
-  const { theme, toggleTheme } = contextValue;
   const data = {
     logo: logo,
     title: 'Intimate',
     message: 'Ciao, sono Otis e sono qua per ripondere ai tuoi dubbi risuardo la tua intimità.'
   };
+  const suggestWidgetsMock = [
+    {
+      title: 'Potrei soffrire di micorpenia?',
+      description: 'Quando sei pronto, ne riparliamo.',
+      image: '../assets/img/masturb.jpg', // Just a placeholder image URL (replace with your actual URLs)
+    },
+    {
+      title: 'Non riesco a capire cosa mi piace.',
+      description: 'E\' un buon momento per chattare? Riprendiamo da dove avevamo interrotto.',
+      image: '../assets/img/masturb.jpg',
+    },
+    {
+      title: 'Perchè non provo piacere quando mi masturbo?',
+      description: 'Quando sei pronto, completiamo insieme la tua valutazione.',
+      image: '../assets/img/masturb.jpg',
+    },
+    {
+      title: 'Come si infila un tampax?',
+      description: 'Felice di averti aiutato :)',
+      image: '../assets/img/masturb.jpg',
+    },
+  ];
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeComponent {...data} />} />
         <Route path="/chat" element={<ChatComponent />} />
+        <Route path="/suggest" element={<SuggestList data={suggestWidgetsMock} />} />
       </Routes>
     </Router>
-    // <div className={`App theme-${theme}`}>
-    //   {/* <h1>OTIS</h1>
-    //   <button  onClick={() => toggleTheme('dark')}>Dark Theme</button>
-    //   <button  onClick={() => toggleTheme('light')}>Light Theme</button>
-    //   <Logo fill='red' stroke='green' width={100} height={100} /> */}
-    //   {/* <HomeComponent {...data}/> */}
-    // </div>
   );
 }
 
---
+++ src/components/suggest.component.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface Widget {
+    title: string;
+    description: string;
+    image: string;
+}
+
+interface WidgetListProps {
+    data: Widget[];
+}
+
+const SuggestList = ({ data }: WidgetListProps) => {
+
+    return (
+        <div className='suggestion-main-content'>
+            <header>
+                <h2 className='suggest-title'>Cosa ci siamo già detti</h2>
+            </header>
+            <div className='suggest-list-content'>
+                {data.map((item, index) => (
+                    <div key={index} className="widget-container">
+                        <div className="image-container">
+                            <img src={item.image} alt={item.title} className="circular-image" />
+                        </div>
+                        <div className="text-container">
+                            <h2>{item.title}</h2>
+                            <p>{item.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default SuggestList;
